3efca93f6b3f254ee4f1e293dd3d4f6ef66ae1d6
Tianye Zhao <TZHAO18@ubishops.ca>
2026-02-01 17:58:26 -0500

    Refactor files

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 const express = require('express');
 
 // Middleware imports
-// const { auth, requiresAuth } = require('express-openid-connect');
+const { auth, requiresAuth } = require('express-openid-connect');
 
-// const auth0Config = require('./config/auth0');
+const auth0Config = require('./config/auth0');
 
-// const noteRouter = require('./routes/noteRoutes');
-// const userRouter = require('./routes/userRoutes');
+const noteRouter = require('./routes/noteRoutes');
 
 const app = express();
 
 // Middlewares
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Auth0 middleware (required for requiresAuth())
-// app.use(auth(auth0Config));
+app.use(auth(auth0Config));
 
 // API Routes - all protected
-app.use('/api/v1/notes', noteRouter);
-// app.use('/api/v1/users', requiresAuth(), userRouter);
+app.use('/api/v1/notes', requiresAuth(),noteRouter);
 
 // Health check endpoint (public)
 app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'ok', app: 'Note API backend', message: 'Note API backend is running!' });
+  res.status(200).json({
+    status: 'success',
+    message: 'Note API backend is running!'
+  });
 });
 
 module.exports = app;
